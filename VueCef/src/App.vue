0603458ff5613ddbd3e6fd4544f2711ae15ce643
ales2026 <ale.urankar@gmail.com>
2025-07-27 19:05:29 +0200

    question tree

--- VueCef/src/App.vue
+++ VueCef/src/App.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { ref, computed } from 'vue'
-
+// api definition
 interface MyApi {
   doVersion(
     text: string,
@@ -8,56 +8,61 @@ interface MyApi {
     reject: (msg: string) => void,
   ): void;
 }
-
 const myApi = window as unknown as MyApi;
-
-function doVersion(text:string): Promise<boolean> {
+function doVersion(text: string): Promise<boolean> {
   return new Promise<boolean>((accept, reject) => myApi.doVersion(text, accept, reject))
 }
-
 // reactive data
-const powerLevel = ref<number|null>(null)
+const answer = ref<string|null>(null)
 const inProgress = ref(false)
 // computed data
-const powerLevelNorm = computed(() => powerLevel.value ?? 0)
+const resultText = computed(() => {
+  if(inProgress.value) {
+    return "Test in progress...";
+  }
+  else if(answer.value === null) {
+    return "NO DATA";
+  }
+  else {
+    return `You have: ${answer.value}`;
+  }
+})
 // functions
-function quest(text: string): Promise<void> {
-  return doVersion(text).then((btn) => {powerLevel.value = btn ? <number>powerLevel.value + 1 : <number>powerLevel.value})
-}
 function doit() {
   inProgress.value = true;
-  powerLevel.value = 0
-
-  doVersion("Question1")
-  .then(() => quest("Question2"))
-  .then(() => quest("Question3"))
-  .then(() => quest("Question4"))
-  .then(() => quest("Question5"))
-  .catch(() => powerLevel.value = null)
+  answer.value = null
+  doVersion("Path 1").then((yes) => {
+    if(yes) {
+      return doVersion("Path 1.1").then((yes) => {
+        if(yes) {
+          answer.value = "1.1";
+        }
+        else {
+          answer.value = "1";
+        }
+      })
+    }
+    else {
+      return doVersion("Path 2.1").then((yes) => {
+        if(yes) {
+          answer.value = "2.1";
+        }
+        else {
+          answer.value = "2";
+        }
+      })
+    }
+  })
   .finally(() => inProgress.value = false)
 }
 </script>
-
-
 
 <template>
   <v-app>
     <v-main>
       <v-container width="1024">
         <h2 class="mb-2 ml-4">Take the Test</h2>
-        <v-card title="Power Level" class="mb-3 pa-3 pt-5">
-          <div class="ma-4">
-            <v-progress-linear
-              v-model="powerLevelNorm"
-              color="blue-darken-3"
-              :indeterminate="powerLevel === null"
-              :stream="inProgress"
-              rounded
-              max="5"
-              height="20"
-            ></v-progress-linear>
-          </div>
-        </v-card>
+        <v-card title="Animal" :text="resultText"></v-card>
         <div class="d-flex justify-center mt-3">
           <v-btn color="purple" @click="doit" size="x-large">JUST DO IT</v-btn>
         </div>
