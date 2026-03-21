e33a010b5a7965bf48765c9c455b1268ed6fbfcb
ales2026 <ale.urankar@gmail.com>
2026-03-21 16:19:20 +0100

    graphics: clear, present, resize, move comPtr variables into header

--- CefApp/src/renderer/Graphics.cpp
+++ CefApp/src/renderer/Graphics.cpp
@@ -1,7 +1,5 @@
 #include "Graphics.h"
 #include <platform/MyWin.h>
-#include <d3d11.h>
-#include <wrl.h>
 
 #pragma comment(lib, "d3d11.lib")
 
@@ -14,23 +12,26 @@ Graphics::Graphics(HWND hwndOverlay)
     sd.BufferDesc.Format = DXGI_FORMAT_B8G8R8A8_UNORM;
     sd.BufferDesc.RefreshRate.Numerator = 0;
     sd.BufferDesc.RefreshRate.Denominator = 0;
+    sd.BufferDesc.Scaling = DXGI_MODE_SCALING_UNSPECIFIED;
+    sd.BufferDesc.ScanlineOrdering = DXGI_MODE_SCANLINE_ORDER_UNSPECIFIED;
     sd.SampleDesc.Count = 1;
     sd.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
     sd.BufferCount = 1;
     sd.OutputWindow = hwndOverlay;
     sd.Windowed = TRUE;
     sd.SwapEffect = DXGI_SWAP_EFFECT_DISCARD;
+    sd.Flags = 0;
 
-    Microsoft::WRL::ComPtr<ID3D11Device> pDevice;
-    Microsoft::WRL::ComPtr<ID3D11DeviceContext> pContext;
-    Microsoft::WRL::ComPtr<IDXGISwapChain> pSwap;
-    Microsoft::WRL::ComPtr<ID3D11RenderTargetView> pTarget;
+    UINT swapCreateFlags = 0u;
+#ifndef NDEBUG
+    swapCreateFlags |= D3D11_CREATE_DEVICE_DEBUG;
+#endif
 
     D3D11CreateDeviceAndSwapChain(
         nullptr,
         D3D_DRIVER_TYPE_HARDWARE,
         nullptr,
-        0,
+        swapCreateFlags,
         nullptr,
         0,
         D3D11_SDK_VERSION,
@@ -40,16 +41,31 @@ Graphics::Graphics(HWND hwndOverlay)
         nullptr,
         &pContext
     );
+
+    Microsoft::WRL::ComPtr<ID3D11Resource> pBackBuffer;
+    pSwap->GetBuffer(0, __uuidof(ID3D11Resource), &pBackBuffer);
+    pDevice->CreateRenderTargetView(pBackBuffer.Get(), nullptr, &pTarget);
 }
 
 void Graphics::Clear(float red, float green, float blue) noexcept
 {
+    const float color[] = { red,green,blue,1.0f };
+    pContext->ClearRenderTargetView(pTarget.Get(), color);
 }
 
 void Graphics::Resize(int width, int height)
 {
+    if (!pSwap) return;
+    pTarget.Reset(); // release current RTV
+    pSwap->ResizeBuffers(0, width, height, DXGI_FORMAT_UNKNOWN, 0);
+
+    Microsoft::WRL::ComPtr<ID3D11Resource> pBackBuffer;
+    pSwap->GetBuffer(0, __uuidof(ID3D11Resource), &pBackBuffer);
+    pDevice->CreateRenderTargetView(pBackBuffer.Get(), nullptr, &pTarget);
 }
 
 void Graphics::Present()
 {
+    if (pSwap)
+        pSwap->Present(1, 0);
 }
--- CefApp/src/renderer/Graphics.h
+++ CefApp/src/renderer/Graphics.h
@@ -1,5 +1,7 @@
 #pragma once
 #include <platform/MyWin.h>
+#include <d3d11.h>
+#include <wrl.h>
 
 class Graphics
 {
@@ -9,4 +11,8 @@ public:
 	void Resize(int width, int height);
 	void Present();
 private:
+	Microsoft::WRL::ComPtr<ID3D11Device> pDevice;
+	Microsoft::WRL::ComPtr<ID3D11DeviceContext> pContext;
+	Microsoft::WRL::ComPtr<IDXGISwapChain> pSwap;
+	Microsoft::WRL::ComPtr<ID3D11RenderTargetView> pTarget;
 };
